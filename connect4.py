43561b195770fb9662244003705d0f7199093edc
Tom <75928067+tommchugh1@users.noreply.github.com>
2023-11-21 21:31:49 +1100

    Update connect4.py

--- connect4.py
+++ connect4.py
@@ -8,3 +8,9 @@ default_setup = {1: ["|               |"],
                 7: ["| 1 2 3 4 5 6 7 |"]}
 
 active_setup = default_setup
+
+print("WELCOME TO CONNECT 4:")
+player1_name = input("Please input player 1 name: ")
+player1_symbol = input("Please input Player 1 symbol: ")
+player2_name = input("Please input player 2 name: ")
+player2_symbol = input("Please input Player 2 symbol: ")
