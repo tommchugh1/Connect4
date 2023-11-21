1687938d80a0b965f40ec9b0c97664914fb39f2a
Tom <75928067+tommchugh1@users.noreply.github.com>
2023-11-21 21:36:40 +1100

    Added startup functions
    Added a startup and reset function

--- connect4.py
+++ connect4.py
@@ -1,4 +1,5 @@
 
+
 default_setup = {1: ["|               |"], 
                 2: ["|               |"], 
                 3: ["|               |"], 
@@ -7,10 +8,22 @@ default_setup = {1: ["|               |"],
                 6: ["|               |"], 
                 7: ["| 1 2 3 4 5 6 7 |"]}
 
-active_setup = default_setup
 
-print("WELCOME TO CONNECT 4:")
-player1_name = input("Please input player 1 name: ")
-player1_symbol = input("Please input Player 1 symbol: ")
-player2_name = input("Please input player 2 name: ")
-player2_symbol = input("Please input Player 2 symbol: ")
+def reset_game():
+    active_setup = default_setup
+
+def startup():
+    print("WELCOME TO CONNECT 4:")
+    player1_name = input("Please input player 1 name: ")
+    player1_symbol = input("Please input Player 1 symbol: ")
+    while len(player1_symbol) > 1:
+        player1_symbol = input("Symbols can only be 1 character, please input again: ")
+    player2_name = input("Please input player 2 name: ")
+    player2_symbol = input("Please input Player 2 symbol: ")
+    while len(player2_symbol) > 1:
+        player2_symbol = input("Symbols can only be 1 character, please input again: ")
+
+reset_game()
+startup()
+
+
